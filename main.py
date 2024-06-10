27c5e669c05d20a8453b9b6da566ad9b65bc88aa
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-10 17:39:18 +0800

    update here

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ def log() :
                 if int(your_age) <= 10 : # * if your age is less than or equal to 10, age is not allowed
                     print("Age is not allowed")
                     exit()
-            else :
-                print()
-        else : 
-            print(f"{your_age} is not a number") # * If your age is string
+                else :
+                    print()
+            else : 
+                print(f"{your_age} is not a number") # * If your age is string
          
     
     Age_()
