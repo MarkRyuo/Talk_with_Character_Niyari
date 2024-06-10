0be189bca8e4bbfcaa97de182f1d8147b200fb04
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-10 17:41:43 +0800

    update here

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def log() :
 
         while True :
 
+            your_age = input("Enter your age: ")
+
             if your_age.isdigit() :
                 if int(your_age) <= 10 : # * if your age is less than or equal to 10, age is not allowed
                     print("Age is not allowed")
@@ -23,8 +25,8 @@ def log() :
                     print()
             else : 
                 print(f"{your_age} is not a number") # * If your age is string
-                
-        return None
+
+        return your_age
          
     
     Age_()
