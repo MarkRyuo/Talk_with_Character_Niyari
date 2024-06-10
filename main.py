844f6524ed6e05d037f6340808e237e6087c6d03
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-10 17:40:53 +0800

    update here

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ def log() :
             if your_age.isdigit() :
                 if int(your_age) <= 10 : # * if your age is less than or equal to 10, age is not allowed
                     print("Age is not allowed")
-                    exit()
+                    break
                 else :
                     print()
             else : 
                 print(f"{your_age} is not a number") # * If your age is string
+                
+        return None
          
     
     Age_()
