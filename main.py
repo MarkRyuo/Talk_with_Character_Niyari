1d53c70b38c4b56ccd844e49c3b5925d2a7d3e00
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-11 17:03:47 +0800

    update here

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def log() :
                 else :
                    break
             else : 
-                 attempt -= 1 # * Attempt Decreament by 1 
+                attempt -= 1 # * Attempt Decreament by 1 
                 if attempt > 0 :  # * If attempt is greater than to 0 
                     print("Try again")
                 else :
