7bbd32b8a7b3668f7864c77192ba0b8a85dbb4f0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-11 17:03:05 +0800

    update here

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def log() :
 
     def Age_() : # * Define a function to age verification 
 
-        attempt = 2 # * Declare a variable attempt  
+        attempt = 2 # * Declare a variable for attempt  
 
         while True : # * While loop 
 
