4c86891ace0271c3bea531736d14f985755b6021
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-13 17:04:02 +0800

    update herE

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ class Character_ :
             f"Welcome to Talk With {CHAR_NAME}"
             )
         print(
-            f"Age Detected: {self.Age}"
+            f"Age Detected: {self.Age} \n 
+            Name Detected: {self.Name}"
         )
     
     def speak1(self) :
