4924607496639d399beadb12005a182bb5950433
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-13 17:11:45 +0800

    update herE

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def log() :
                 if int(your_age) <= 10 : # * if your age is less than or equal to 10, age is not allowed
                     print("Age is not allowed")
                     break
+                    exit
                 else : # * Break if greater than 10 
                     break
             else : 
